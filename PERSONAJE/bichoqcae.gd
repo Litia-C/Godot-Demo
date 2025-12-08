1877c2297a57c4c37df4b77aab0bc0463f0ae8d6
Litia-C <litia.calderon@gracia.lasalle.cat>
2025-12-08 20:28:11 +0100

    SpriteAnimado
    Estaba aburrida y he aprendido como cambiar de sprites

--- PERSONAJE/bichoqcae.gd
+++ PERSONAJE/bichoqcae.gd
@@ -5,26 +5,26 @@ var dragging = false
 var of = Vector2(0,0)
 
 @onready var timer: Timer = $Timer
-
+# Función Caer
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if dragging:
+		$AnimatedSprite2D.play("grabbed")
 		position = get_global_mouse_position()
 		
-	if not is_on_floor():
-		velocity += get_gravity() * (delta*0.09)
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Gravedad 
+	velocity += get_gravity() * (delta*0.099)
+	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
 
 
 func _on_button_button_down() -> void:
 	dragging = true
 	timer.start()
 func _on_timer_timeout() -> void:	
-	dragging = false	
-	print("waos")
+	dragging = false
+	$AnimatedSprite2D.play("falling")	
 
 func _on_button_button_up() -> void:
 	dragging = false
