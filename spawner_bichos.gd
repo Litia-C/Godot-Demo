735bf909a3faeac2f8a4a18e4ca7887db973bdfe
Litia-C <litia.calderon@gracia.lasalle.cat>
2026-01-12 09:34:45 +0100

    Chore: Spawner malo pero funciona

--- spawner_bichos.gd
+++ spawner_bichos.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-var bicho_codigo = preload("res://PERSONAJE/Bichoqcae.tscn")
+
+@onready var main = get_node("..")
 @onready var timer: Timer = $Timer
+var bicho_scene := preload("res://PERSONAJE/Bichoqcae.tscn")
+
 func _ready() -> void:
-	timer.start()	
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	var bicho = bicho_scene.instantiate()
+	bicho.position = position
+	main.add_child(bicho)
