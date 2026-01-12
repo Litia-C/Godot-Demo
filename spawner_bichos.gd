5a4ed4343ab05745968aae54af8fe4c79e697366
Litia-C <litia.calderon@gracia.lasalle.cat>
2026-01-12 09:44:02 +0100

    fix: spawner aleatorio

--- spawner_bichos.gd
+++ spawner_bichos.gd
@@ -3,12 +3,19 @@ extends Node2D
 
 @onready var main = get_node("..")
 @onready var timer: Timer = $Timer
-var bicho_scene := preload("res://PERSONAJE/Bichoqcae.tscn")
-
+var bicho_scene = preload("res://PERSONAJE/Bichoqcae.tscn")
+var puntos_spawn = []
 func _ready() -> void:
+	for i in get_children():
+		if i is Marker2D:
+			puntos_spawn.append(i)
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	#Puntos de spawn aleatorios
+	var punto = puntos_spawn[randi() % puntos_spawn.size()]
+	
+	
 	var bicho = bicho_scene.instantiate()
-	bicho.position = position
+	bicho.position = punto.position
 	main.add_child(bicho)
